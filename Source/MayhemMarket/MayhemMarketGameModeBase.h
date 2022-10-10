3adf3a652348653b84999b711496764e15517f71
Jimmy <jkchuong@gmail.com>
2022-10-10 11:13:10 +0100

    Initial Commit

---
+++ Source/MayhemMarket/MayhemMarketGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MayhemMarketGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAYHEMMARKET_API AMayhemMarketGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
