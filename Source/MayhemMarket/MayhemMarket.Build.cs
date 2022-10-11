2b9775b8cb17d07df2b6984580f0eba28e5df15f
Jimmy <jkchuong@gmail.com>
2022-10-11 18:17:06 +0100

    begin player HUD

--- Source/MayhemMarket/MayhemMarket.Build.cs
+++ Source/MayhemMarket/MayhemMarket.Build.cs
@@ -8,7 +8,7 @@ public class MayhemMarket : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
