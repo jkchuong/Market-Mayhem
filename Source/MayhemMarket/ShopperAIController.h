9bc2c9232082669b0cacb234f4b57c0297c5d768
Jimmy <jkchuong@gmail.com>
2022-10-14 11:55:11 +0100

    Begin AI Controller

---
+++ Source/MayhemMarket/ShopperAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "ShopperAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAYHEMMARKET_API AShopperAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
